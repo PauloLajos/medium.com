e734ce488e9291433523650defb3585a8bef0f3f
PauloLajos <paulolajos@gmail.com>
2023-12-03 06:48:46 +0100

    Secure Keys - start project

---
+++ SecureKeys/app/src/main/java/com/cabmancode/securekeys/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cabmancode.securekeys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
