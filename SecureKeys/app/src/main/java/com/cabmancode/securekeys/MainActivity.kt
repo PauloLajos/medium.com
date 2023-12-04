97cc099bc70bcee0f274880ba36d52356343d8f8
PauloLajos <paulolajos@gmail.com>
2023-12-04 08:09:23 +0100

    Secure Keys - add secrets-gradle-plugin - update gradle plugin to 8.2

--- SecureKeys/app/src/main/java/com/cabmancode/securekeys/MainActivity.kt
+++ SecureKeys/app/src/main/java/com/cabmancode/securekeys/MainActivity.kt
@@ -2,10 +2,18 @@ package com.cabmancode.securekeys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    // Read API_KEY for build file
+    private val apiKey = BuildConfig.API_KEY
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView : TextView = findViewById<TextView>(R.id.textView1)
+        textView.text = apiKey
     }
 }
